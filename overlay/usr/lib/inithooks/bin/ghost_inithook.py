339f20427340df7520797aef7787a0edf65cc68a
Rik Goldman <rikgoldman@gmail.com>
2016-04-26 06:40:09 -0400

    revert inithook

--- overlay/usr/lib/inithooks/bin/ghost_inithook.py
+++ overlay/usr/lib/inithooks/bin/ghost_inithook.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python
-"""Set Ghost email, name, address, password
+"""Set Ghost email, name, URL, password
 
 Option:
 --password= unless provided, will ask interactively
 --email= unless provided, will ask interactively
---username= unless provided, will ask interactively
+--addy= unless provided, will ask interactively
+--uname= unless provided, will ask interactively
+
 """
 
 import sys
@@ -28,12 +30,13 @@ def usage(s=None):
 def main():
     try:
         opts, args = getopt.gnu_getopt(sys.argv[1:], "h",
-                                       ['help', 'password=', 'email=', 'username='])
+                                       ['help', 'password=', 'email=', 'addy=', 'uname='])
     except getopt.GetoptError, e:
         usage(e)
 
     password = ""
     email = ""
+    addy = ""
     uname = ""
 
     for opt, val in opts:
@@ -43,6 +46,8 @@ def main():
             password = val
         elif opt == '--email':
             email = val
+	elif opt == '--addy':
+	    addy = val
         elif opt == '--username':
             uname = val
 
@@ -52,9 +57,17 @@ def main():
 
     if not email:
         if 'd' not in locals():
-			d = Dialog('TurnKey Linux - First boot configuration')
+            d = Dialog('TurnKey Linux - First boot configuration')
 
         email = d.get_email("Ghost Email","Enter email address for the Ghost blogger account.","admin@example.com")
+
+    if not addy:
+        if 'd' not in locals():
+            d = Dialog('Turnkey Linux - First boot configuration')
+        addy = d.get_input(
+            "Ghost URL (not IP address)",
+            "Enter the full URL of the Ghost Blog.",
+            "http://my-ghost-blog.org")
 
     if not uname:
         if 'd' not in locals():
@@ -65,24 +78,30 @@ def main():
             "Enter the Ghost blogger's name (real name recommended).",
             "Blogger Unknown")
 
+#Kept for reference
+#hash = hashlib.md5(password).hexdigest()
+
     hash = bcrypt.hashpw(password,bcrypt.gensalt())
 
+
+
     dbase = "/opt/ghost/content/data/ghost.db"
-    #uid = "1"
+    uid = "1"
     con = lite.connect(dbase)
     with con:
         cur = con.cursor()
-        #cur.execute('UPDATE roles_users SET user_id="4" WHERE id="1";')
+
+        cur.execute('UPDATE roles_users SET role_id="4" WHERE id="1";')
+	cur.execute('UPDATE roles_users SET user_id="1" WHERE id="1";')
         cur.execute('UPDATE users SET password=\"%s\" WHERE id="1";' % hash)
         cur.execute('UPDATE users SET name=\"%s\" WHERE id="1";' % uname)
         cur.execute('UPDATE users SET email=\"%s\" WHERE id="1";' % email)
         cur.execute('UPDATE users SET status=\"active\" WHERE id="1";')
-
         con.commit()
 
-    #for line in fileinput.FileInput("/opt/ghost/config.js",inplace=1):
-    #    line = line.replace("http://my-ghost-blog.com", address)
-    #    print line
+    for line in fileinput.FileInput("/opt/ghost/config.js",inplace=1):
+        line = line.replace("http://my-ghost-blog.com",addy)
+        print line
 
 if __name__ == "__main__":
     main()
