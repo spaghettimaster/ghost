cd8b80a13ed5d0ae26010257d4a6ca122af485c1
Rik Goldman <rikgoldman@gmail.com>
2016-05-03 21:20:27 -0400

    add python to setup ghost to email with sendmail
    I'm sure there's a better place for this...a better way to implement it. I'm just not sure where to put it and how to call it.

---
+++ conf.d/ghost-mail.py
@@ -0,0 +1,19 @@
+#! /usr/bin/python
+
+import fileinput
+import re
+
+config = "./config.js"
+old = "mail: {},"
+new = """
+        mail: {
+            transport: 'SMTP',
+            options: {
+                service: 'sendmail',
+            }
+        },
+"""
+
+for line in fileinput.input(config,inplace=1):
+    line = line.replace(old,new)
+    print line,
