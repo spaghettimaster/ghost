2a6ab1e64d6021bdec99794634b5155338782112
Jeremy Davis <jeremy@turnkeylinux.org>
2016-05-09 00:26:14 +1000

    reworking inithooks - still in progress

---
+++ overlay/usr/lib/inithooks/bin/ghost_in-progress.py
@@ -0,0 +1,142 @@
+#!/usr/bin/python
+"""Set Ghost email, name, password, address
+
+Option:
+    --pass=     unless provided, will ask interactively
+    --email=    unless provided, will ask interactively
+    --uname=    unless provided, will asl interactively
+                DEFAULT="Blogger Unknown"
+    --domain=   unless provided, will ask interactively
+                DEFAULT="www.example.com"
+"""
+
+import sys
+import getopt
+import subprocess
+import hashlib
+import sqlite3 as lite
+import locale
+import dialog
+import fileinput
+from dialog_wrapper import Dialog
+import re
+import validators
+import subprocess
+
+DEFAULT_USER = "Blogger Unknown"
+DEFAULT_DOMAIN = "www.example.com"
+
+def usage(s=None):
+    if s:
+        print >> sys.stderr, "Error:", s
+    print >> sys.stderr, "Syntax: %s [options]" % sys.argv[0]
+    print >> sys.stderr, __doc__
+    sys.exit(1)
+
+def main():
+    try:
+        opts, args = getopt.gnu_getopt(sys.argv[1:], "h",
+                                       ['help', 'password=', 'email=', 'uname=', 'address='])
+    except getopt.GetoptError, e:
+        usage(e)
+
+    pass = ""
+    email = ""
+    uname = ""
+    domain = ""
+
+    for opt, val in opts:
+        if opt in ('-h', '--help'):
+            usage()
+        elif opt == '--pass':
+            pass = val
+        elif opt == '--email':
+            email = val
+        elif opt == '--name':
+            uname = val
+        elif opt == '--address':
+            address = val
+
+    if not domain:
+        offer = "https://example.com"
+        valid = ""
+        config = "/opt/ghost/config.js"
+
+        def check_url(addy):
+            validity = validators.url(addy)
+            return validity
+
+
+        def address():
+            d = Dialog('TurnKey Linux - First boot configuration')
+
+            addy = d.get_input("Ghost URL", "Enter the URL for the new Ghost blog (https recommended). If the URL is not valid, please try again.", "https://example.com")
+            return addy
+
+
+        def get_url(filein):
+            f = open(filein, 'r')
+            f = f.read()
+            urls = re.findall(r'(http[s]?://[^\s]+)', f)
+            current_url = urls[2]
+            current_url = current_url.translate(None, "',")
+            return current_url
+
+        def replace_url(old,new,fileout):
+            config = None
+            with open(fileout,'r') as file :
+                config = file.read()
+            config = config.replace(old,new)
+            with open(fileout, 'w') as file:
+                file.write(config)
+            return 0
+
+        while not valid:
+            addy = address()
+            valid = check_url(addy)
+
+        current_url = get_url(config)
+        replace_url(current_url,addy,config)
+
+
+    if not password:
+        d = Dialog('TurnKey Linux - First boot configuration')
+        password = d.get_password("Ghost Password","Enter new password for the Ghost blogger account (>= 8 characters).")
+
+    if not email:
+        if 'd' not in locals():
+            d = Dialog('TurnKey Linux - First boot configuration')
+
+        email = d.get_email("Ghost Email","Enter email address for the Ghost blogger account.","admin@example.com")
+
+    if not uname:
+        if 'd' not in locals():
+            d = Dialog('Turnkey Linux - First boot configuration')
+
+        uname = d.get_input(
+            "Ghost Account Name",
+            "Enter the Ghost blogger's name (real name recommended).",
+            "Blogger Unknown")
+
+
+        #slug = uname.replace(" ", "-")
+
+
+    hash = bcrypt.hashpw(password,bcrypt.gensalt())
+
+
+    dbase = "/opt/ghost/content/data/ghost.db"
+    con = lite.connect(dbase)
+    with con:
+        cur = con.cursor()
+
+        cur.execute('UPDATE users SET password=\"%s\" WHERE id="1";' % hash)
+        cur.execute('UPDATE users SET name=\"%s\" WHERE id="1";' % uname)
+        cur.execute('UPDATE users SET email=\"%s\" WHERE id="1";' % email)
+        cur.execute('UPDATE users SET status=\"active\" WHERE id="1";')
+        #cur.execute('UPDATE users SET slug=\"%s\" WHERE id="1";' % slug)
+        con.commit()
+
+
+if __name__ == "__main__":
+    main()
